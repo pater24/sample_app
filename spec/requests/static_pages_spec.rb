d6f3c22b533920c29fc3a1ff485af8ed24a0bb01
pater24 <peter.tarectecan@gmail.com>
2014-07-22 12:23:18 -0400

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,6 +32,38 @@ describe "Static pages" do
       click_link "sample app"
       expect(page).to have_title(full_title(''))
     end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "pagination" do
+        before do
+          30.times { FactoryGirl.create(:micropost, user: user) }
+          visit root_path
+        end
+        after { user.microposts.delete_all }
+
+        it { should have_selector('div.pagination') }
+
+        it "should list each micropost" do
+          user.feed.paginate(page: 1).each do |item|
+            expect(page).to have_selector('li', text: item.content)
+          end
+        end
+      end
+    end
   end
 
   describe "Help page" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,10 +57,34 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+
+      describe "pagination" do
+        before do
+          30.times { FactoryGirl.create(:micropost, user: user) }
+          visit user_path(user)
+        end
+        after { user.microposts.delete_all }
+
+        it { should have_selector('div.pagination') }
+
+        it "should list each micropost" do
+          user.microposts.paginate(page: 1).each do |micropost|
+            expect(page).to have_selector('li', text: micropost.content)
+          end
+        end
+      end
+    end
   end
 
   describe "signup page" do
